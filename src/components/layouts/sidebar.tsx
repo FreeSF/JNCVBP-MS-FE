4cb1e6ad7eef33120735d2e14970a58456793f64
Jerson Paniagua <diazpany@gmail.com>
2021-09-01 20:23:50 -0400

    add menus and custom routes

---
+++ src/components/layouts/sidebar.tsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { useLocation, NavLink } from "react-router-dom";
+
+import { Nav } from "react-bootstrap";
+
+import logo from "assets/img/reactlogo.png";
+import sidebarImage from "assets/img/sidebar-3.jpg";
+
+function Sidebar({ color, image, routes }) {
+  const location = useLocation();
+  const activeRoute = (routeName) => {
+    if (routeName == '/') {
+      return location.pathname == '/' ? "active" : "";
+    }
+    return (location.pathname.indexOf(routeName) > -1) ? "active" : "";
+  };
+
+  return (
+    <div className="sidebar" data-image={image} data-color={color}>
+      <div
+        className="sidebar-background"
+        style={{
+          backgroundImage: `url(${require("../../assets/img/sidebar-3.jpg")})`,
+        }}
+      />
+      <div className="sidebar-wrapper">
+        <div className="logo d-flex align-items-center justify-content-start">
+          <a
+            href="/"
+            className="simple-text logo-mini mx-1"
+          >
+            <div className="logo-img">
+              <img
+                src={require("../../assets/img/reactlogo.png")}
+                alt="..."
+              />
+            </div>
+          </a>
+          <a className="simple-text" href="/">
+            JNCVBP-MS
+          </a>
+        </div>
+        <Nav>
+          {routes.map((prop, key) => {
+            if (!prop.redirect && prop.showOnSidebar)
+              return (
+                <li className={activeRoute(prop.path)} key={key}>
+                  <NavLink
+                    to={prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            return null;
+          })}
+        </Nav>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
