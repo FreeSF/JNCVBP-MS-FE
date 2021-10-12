6ca36ca947e7b5a68ad34fd9190f94e3112ea180
Alex Jines <abel.oalex@gmail.com>
2021-10-11 23:27:40 -0300

    added constants for create and edit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
 // develop site 'https://jncvbp-ms.herokuapp.com/graphql';
 export const API_URL = 'https://jncvbp-ms.herokuapp.com/graphql';
+
+// Can be moved up to make the constants
+export const MODE_CREATE = 'CREATE';
+export const MODE_EDIT = 'EDIT';
