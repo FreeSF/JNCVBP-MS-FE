f8a9ba1be9bcbcbddfe6367a7df1008009298a88
Jerson Paniagua <diazpany@gmail.com>
2022-03-23 11:35:33 -0300

    minor

--- src/components/Duties/UpdateDutyPage.tsx
+++ src/components/Duties/UpdateDutyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { useMutation, useQuery } from "react-apollo";
 
@@ -9,10 +9,6 @@ import { Container } from "react-bootstrap";
 import { EditDutyMutation, EditDutyMutationVariables, FindDutyQuery, UpdateDutyInput } from "../../types";
 import { EDIT_DUTY, FIND_DUTY, GET_DUTIES } from "../../queries/duties";
 import DutyForm from "./DutyForm";
-
-interface theProps extends RouteComponentProps {
-  id: String;
-}
 
 const UpdateDutyPage = (props: RouteComponentProps<{ id: string }>) => {
   const getDuty = useQuery<FindDutyQuery>(FIND_DUTY, { variables: { id: props.match.params.id } });
