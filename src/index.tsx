4abef61ae88625640beb0ee2f142ceeed04ca575
Jerson Paniagua <diazpany@gmail.com>
2022-07-31 19:06:10 -0400

    remove import

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "./assets/css/demo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import { setContext } from "@apollo/client/link/context";
 import { AUTH_TOKEN_NAME } from "./utils/constants";
 
