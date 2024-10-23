d9f654b0473af34fa0738727ef75ac105b2350f4
Alex Jines <abel.oalex@gmail.com>
2024-10-23 15:38:57 -0300

    added comments for homepage and spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "../stylesheets/spinner.sass";
 
+/**
+ * Spinner component description
+ *
+ * Simple spinner component that will be displayed until the data is loaded from GraphQL API
+ */
 const Spinner = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
